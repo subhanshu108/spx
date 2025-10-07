6f3321bfa63cf726d16ad16d001931e1b7e9ef34
subhanshu108 <subhanshuprasad2002@gmail.com>
2025-10-07 17:02:51 +0530

    added UserService.java

---
+++ src/main/java/com/amdocs/spx/service/UserService.java
@@ -0,0 +1,31 @@
+package com.amdocs.spx.service;
+
+
+import com.amdocs.spx.entity.User;
+import com.amdocs.spx.repository.TicketTypeRepository;
+import com.amdocs.spx.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+    public List<User> getAllUsers(String name){
+        return userRepository.findAll();
+    }
+
+
+}
