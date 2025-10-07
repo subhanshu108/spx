2552dc1aac8b782c2f437caa72a683ed9099e75a
subhanshu108 <subhanshuprasad2002@gmail.com>
2025-10-07 18:42:45 +0530

    changed OrderItemRepository.java

--- src/main/java/com/amdocs/spx/repository/OrderItemRepository.java
+++ src/main/java/com/amdocs/spx/repository/OrderItemRepository.java
@@ -1,9 +1,10 @@
 package com.amdocs.spx.repository;
 
+import com.amdocs.spx.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface OrderItemRepository extends JpaRepository<OrderItemRepository, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
