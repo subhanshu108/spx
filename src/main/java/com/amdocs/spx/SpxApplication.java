8994291d611ba3534fd05ca6327e89d5e6d41976
subhanshu108 <subhanshuprasad2002@gmail.com>
2025-10-07 15:48:06 +0530

    Initial commit

---
+++ src/main/java/com/amdocs/spx/SpxApplication.java
@@ -0,0 +1,13 @@
+package com.amdocs.spx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amdocs/spx/SpxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.spx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
